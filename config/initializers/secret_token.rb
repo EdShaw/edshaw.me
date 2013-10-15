2423aa0bec966fd5b12edf4ee6fb6251d8e6cc00
Edward Shaw <edward.shaw@worc.ox.ac.uk>
2013-10-15 14:27:26 +0100

    Hello, edshaw.net

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EdshawMe::Application.config.secret_key_base = '7f5f9a0ea11a0dd4baee4d372f527e2e828bcc99b989928c1ab0c6103d3c7b9047d4f14d943c7276e422091abb16bd7361fc984f326b692a044f1efaf4d633a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EdshawMe::Application.config.session_store :cookie_store, key: '_edshaw_me_session'
