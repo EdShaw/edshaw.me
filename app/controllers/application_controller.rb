bc1cd60c632eaaff0eb79de45a4968327bd17760
Edward Shaw <edward.shaw@worc.ox.ac.uk>
2013-10-15 17:41:42 +0100

    Test update.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    render text: "Hello Edd"
+    render text: "Hello, World!"
   end
 
 end
